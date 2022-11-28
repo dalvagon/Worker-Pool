3b180b86fd4585c8d8467b176fc17c29a8f6da2a
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2022-11-28 11:43:42 +0200

    Added exception handling for KeyboardInterrupt

--- master.py
+++ master.py
@@ -1,26 +1,29 @@
 import os
 import logging
 import pika
-from get_countries import get_countries
-from colors import MAGENTA, BLUE, CYAN, GREEN, YELLOW, RED, ENDC, BOLD, UNDERLINE
+from util.get_countries import get_countries
+from util.colors import MAGENTA, BLUE, CYAN, GREEN, YELLOW, RED, ENDC, BOLD, UNDERLINE
 
 CONNECTION_STRING = "sites_queue"
 
 
 def create_queue():
     """Creates the queue by pushing page links and the page download locations onto it"""
-
     logging.info(MAGENTA + BOLD + "Creating connection for queue..." + ENDC)
+
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
     channel.queue_delete(queue=CONNECTION_STRING)
     channel.queue_declare(queue=CONNECTION_STRING)
+
     logging.info(GREEN + BOLD + "Connection created" + ENDC)
 
     countries = get_countries()
+    countries.sort()
     for country in countries:
-        url = f"google.com/{country}"
-        location = os.path.join(".", f"{country}")
+        country_name = country.replace(" ", "")
+        url = f"https://www.infoplease.com/countries/{country_name}"
+        location = os.path.join(".", "pages", f"{country_name}")
         logging.info(
             MAGENTA
             + BOLD
@@ -32,7 +35,7 @@ def create_queue():
             + ENDC
             + MAGENTA
             + BOLD
-            + " onto the queue. The page witll be downloaded in "
+            + " onto the queue. The page will be downloaded in "
             + ENDC
             + BLUE
             + UNDERLINE
@@ -54,4 +57,9 @@ def create_queue():
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
-    create_queue()
+    try:
+        create_queue()
+    except KeyboardInterrupt:
+        logging.error(MAGENTA + BOLD + "Interrupted" + ENDC)
+    except Exception as e:
+        logging.error(str(e))
--- worker.py
+++ worker.py
@@ -1,34 +1,45 @@
 import pika
-import time
-import random
 import json
+import logging
+from util.colors import MAGENTA, BLUE, CYAN, GREEN, YELLOW, RED, ENDC, BOLD, UNDERLINE
+from util.download_page import download_page
+
+CONNECTION_STRING = "sites_queue"
 
 
 def handle_message(ch, method, properties, body):
+    """Callback function to handle a mesaage from the queue"""
     message = json.loads(body.decode("UTF8").replace("'", '"'))
-    print(message["url"])
-    processing_time = random.randint(1, 10)
-    print(
-        f"Received message: {message} will take {processing_time} seconds to complete"
+    logging.info(
+        MAGENTA + BOLD + "Received message : " + ENDC + CYAN + str(message) + ENDC
     )
-    time.sleep(processing_time)
-
+    download_page(message["url"], message["location"])
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    print(f"Finished processing the message {message}")
+    logging.info(MAGENTA + BOLD + "Finished processing the message\n" + ENDC)
 
 
 def consume():
-    CONNECTION_STRING = "sites_queue"
+    """Consumes the messages pushed by the master onto the queue"""
+    logging.info(MAGENTA + BOLD + "Connecting..." + ENDC)
 
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
     channel.queue_declare(queue=CONNECTION_STRING)
     channel.basic_qos(prefetch_count=1)
 
-    channel.basic_consume(queue=CONNECTION_STRING, on_message_callback=handle_message)
+    logging.info(GREEN + BOLD + "Connection established" + ENDC)
 
+    logging.info(GREEN + BOLD + "Started consuming..." + ENDC)
+
+    channel.basic_consume(queue=CONNECTION_STRING, on_message_callback=handle_message)
     channel.start_consuming()
 
 
 if __name__ == "__main__":
-    consume()
+    logging.getLogger().setLevel(logging.INFO)
+    try:
+        consume()
+    except KeyboardInterrupt:
+        logging.error(MAGENTA + BOLD + "Interrupted" + ENDC)
+    except Exception as e:
+        logging.error(str(e))
